8e7c159b6330b9285097ef75c2b8a5cd52cb78f8
devsourceid <35456489+devsourceid@users.noreply.github.com>
2022-03-09 09:48:41 +0700

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,7 +8,7 @@
         <!-- Main Hero Content -->
         <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
             <h1 class="text-4xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-5xl lg:text-6xl"><span class="inline">Semarmesem</span><span class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 to-yellow-400 md:inline">.art</span></h1>
-            <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">Bring the Javanesse mythology into the Blockchain. Created by professional Graphic Designer team, exclusive on Ethereum Blockchain. Just 1 of 1 edition every art.</div>
+            <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">Bring the cultural Javanesse mythology into the Blockchain. Created by professional Graphic Designer team, exclusive on Ethereum Blockchain. Just 1 of 1 edition every art.</div>
             <div class="flex flex-col items-center mt-12 text-center">
                 <span class="relative inline-flex w-full md:w-auto">
                     <a href="#_" type="button" class="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-yellow-500 border border-transparent rounded-full md:w-auto hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
