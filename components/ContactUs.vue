b8e71e58ff8d4768380392caacec900fc6c29ae2
devsourceid <35456489+devsourceid@users.noreply.github.com>
2022-03-10 20:51:47 +0700

    Update ContactUs.vue

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -1,7 +1,7 @@
 <template>
     
 <!-- Section 1 -->
-<section class="py-20 bg-white">
+<section class="py-20 bg-white border-b bprder-gray-200">
     <div class="px-4 mx-auto text-center max-w-7xl sm:px-6 lg:px-8">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl md:text-5xl xl:text-6xl">
             Business Inquiries
@@ -10,7 +10,7 @@
             Want to working with us? Fell free to send us an email, and lets discuss the projects.
         </p>
         <div class="flex justify-center mt-8 space-x-3">
-            <a href="mailto:semarmesemnft@gmail.com" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-yellow-600 border border-transparent rounded-full shadow hover:bg-indigo-700">Contact Us</a>
+            <a href="mailto:semarmesemnft@gmail.com" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-yellow-600 border border-transparent rounded-full shadow hover:bg-yellow-700">Contact Us</a>
         </div>
     </div>
 </section>
